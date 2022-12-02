eb0bba8d47e599207b6218dcc0129495f0b806bb
RJ <neontty@gmail.com>
2022-12-02 15:38:21 -0600

    google s2geometry import working in sample project

---
+++ build.sbt
@@ -0,0 +1,1 @@
+libraryDependencies += "com.google.geometry" % "s2-geometry" % "2.0.0"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+
+package com.near.s2geometry
+
+import com.google.common.geometry.S2.M_PI_2;
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+
+  }
+}
